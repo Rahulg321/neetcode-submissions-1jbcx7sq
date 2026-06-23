261cc3bc9aa6b802e3863b4ac476df5a46ef1b08
Rahul Gupta <rg5353070@gmail.com>
2026-06-23 17:15:35 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let encoded = ""
+        for (let s of strs){
+            encoded += String(s.length) + "#" + s;
+        }
+
+        return encoded;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let res = []
+        let i = 0;
+
+        while(i < str.length){
+            let j = i;
+
+
+            while(str[j] !== "#"){
+                j ++;
+            }
+
+            let length = parseInt(str.substring(i, j));
+            i = j + 1;
+            j = i + length;
+
+            res.push(str.substring(i, j))
+            i = j;
+
+        }   
+
+        return res;
+    }
+}
