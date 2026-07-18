191ab2d7e6043b6885992737c4733804b62f6f9c
Rahul Gupta <rg5353070@gmail.com>
2026-07-18 13:54:21 +0530

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.js
@@ -0,0 +1,36 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node) {
+        const oldToNew = new Map();
+        return this.dfs(node, oldToNew);
+    }
+
+    dfs(node, oldToNew) {
+        if (!node) return null;
+
+        if (oldToNew.has(node)) return oldToNew.get(node);
+
+        const copy = new Node(node.val);
+
+        oldToNew.set(node, copy);
+
+        for (const neighbor of node.neighbors) {
+            copy.neighbors.push(this.dfs(neighbor, oldToNew));
+        }
+
+        return copy;
+    }
+}
