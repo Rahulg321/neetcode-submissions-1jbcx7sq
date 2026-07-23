b6c19af28ed48a5b2ca0f17bcf4446db25acdf10
Rahul Gupta <rg5353070@gmail.com>
2026-07-23 17:38:08 +0530

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canJump(nums) {
+        let goal = nums.length - 1;
+        
+        for(let i = nums.length - 1; i >= 0; i --){
+            if(i + nums[i] >= goal){
+                goal = i;
+            };
+
+        }
+
+        return goal === 0
+
+    }
+}
