38c7bb18c612cc4d2ea9180a282d5a34a31a1334
Rahul Gupta <rg5353070@gmail.com>
2026-07-28 17:39:12 +0530

    Add: non-overlapping-intervals - submission-0

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number}
+     */
+    eraseOverlapIntervals(intervals) {
+        
+        intervals.sort((a, b) => a[0] - b[0]);
+        let result = 0;
+        let prevEnd = intervals[0][1]
+
+
+        for(let i = 1; i < intervals.length; i ++){
+            if(intervals[i][0] >= prevEnd){
+                prevEnd = intervals[i][1]
+            } else{
+                result += 1;
+                prevEnd = Math.min(intervals[i][1], prevEnd)
+            }
+        }
+
+
+        return result;
+        
+    }
+}
